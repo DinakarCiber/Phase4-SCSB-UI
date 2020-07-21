0f477dc1b73b4fdee961e60270b4e3d20525c0d3
reachram <reachramanujam79@gmail.com>
2020-07-20 21:58:29 -0400

    Removed the unused import

--- src/main/java/org/recap/helper/RestTemplateHelper.java
+++ src/main/java/org/recap/helper/RestTemplateHelper.java
@@ -3,7 +3,6 @@ package org.recap.helper;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.recap.model.reports.ReportsRequest;
 import org.recap.util.HelperUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
